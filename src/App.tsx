65e023f0a73d8adac2278289607293b7ea412873
tavosansal <gustavoasanjose@gmail.com>
2021-01-19 00:36:31 -0600

    [FEATURE] Added some styling

--- src/App.tsx
+++ src/App.tsx
@@ -41,13 +41,19 @@ function App() {
       <div className="bg-white p-6 rounded-lg">
         <h2>React Issues</h2>
         {reactIssues.map(item => (
-          <div key={item.id} className="bg-white p-6 rounded-lg border">
-            <h3 className="font-semibold text-md">{item.title}</h3>
+          <div key={item.id} className="bg-white p-4 rounded-lg border hover:bg-gray-200">
 
-            <p>
-              {item.user.login}
-              <img src={item.user.avatar_url} alt="avatar"/>
-            </p>
+           <a className="flex items-center space-x-3.5 sm:space-x-5 lg:space-x-3.5 xl:space-x-5" href={item.html_url} target="_blank">
+              <img src={item.user.avatar_url} alt="avatar" width="160" height="160" className="flex-none w-20 h-20 rounded-lg bg-gray-100" />
+              <div className="min-w-0 flex-auto space-y-0.5">
+                <h2 className="text-base sm:text-xl lg:text-base xl:text-xl font-semibold truncate">
+                  {item.title} (#{item.number})
+                </h2>
+                <a className="text-gray-500 text-base text-sm hover:underline" href={`https://github.com/${item.user.login}`} target="_blank" rel="noopener noreferrer">
+                  Opened By: <span className="text-md font-bold">{item.user.login}</span>
+                </a>
+              </div>
+            </a>
           </div>
         ))}
       </div>
