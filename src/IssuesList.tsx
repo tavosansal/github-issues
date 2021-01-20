bc2320ca4b45d9cf8fd7a016c790eebd65ea26f6
tavosansal <gustavoasanjose@gmail.com>
2021-01-19 19:42:33 -0600

    [CHORE] A bit of cleanup

--- src/IssuesList.tsx
+++ src/IssuesList.tsx
@@ -33,24 +33,32 @@ export default function IssuesList({ title, repoUrl }: IssuesListProps) {
       setIssuesList(cleanupAndSortResults(await results.json()));
     }
     fetchData();
-  }, []);
-
+  });
 
   return (
-    <div className="bg-white p-6 rounded-lg">
-      <h2>{title} Issues</h2>
+    <div className="bg-white p-6 rounded-lg mb-10">
+      <h2 className="font-bold text-xl mb-5">
+        {title} Issues
+      </h2>
       {issuesList.map(item => (
         <div key={item.id} className="bg-white p-4 rounded-lg border hover:bg-gray-200">
           <a className="flex items-center space-x-3.5 sm:space-x-5 lg:space-x-3.5 xl:space-x-5" href={item.html_url} target="_blank" rel="noopener noreferrer">
             <img src={item.user.avatar_url} alt="avatar" width="160" height="160" className="flex-none w-20 h-20 rounded-lg bg-gray-100" />
             <div className="min-w-0 flex-auto space-y-0.5">
-              <h2 className="text-base sm:text-xl lg:text-base xl:text-xl font-semibold truncate">
+              <h3 className="text-base sm:text-xl lg:text-base xl:text-lg font-semibold truncate">
                 {item.title} (#{item.number})
-              </h2>
+              </h3>
               <a className="text-gray-500 text-base text-sm hover:underline" href={`https://github.com/${item.user.login}`} target="_blank" rel="noopener noreferrer">
-                Opened By: <span className="text-md font-bold">{item.user.login}</span>
+                Opened By: 
+                <span className="text-md font-bold">
+                  {item.user.login}
+                </span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="18" height="18">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                </svg>
               </a>
             </div>
+            <button className="rounded-lg bg-red-600 p-2 text-white hover:bg-red-500">Remove</button>
           </a>
         </div>
       ))}
